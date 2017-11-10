4de06315959a6f6a52e569b4b9dcf8e1b4e9854a
lina <lina.krantz@gmail.com>
2017-11-10 19:11:19 +0100

    more gets from db

--- src/main/java/wicket/db/jdbi/UserQueries.java
+++ src/main/java/wicket/db/jdbi/UserQueries.java
@@ -6,13 +6,21 @@ import org.skife.jdbi.v2.sqlobject.customizers.RegisterMapper;
 import wicket.core.entity.User;
 import wicket.core.mapper.UserMapper;
 
+import java.util.List;
+
 @RegisterMapper(UserMapper.class)
 public interface UserQueries {
 
     @SqlQuery("select * from user where userid = :userid")
-    User findByUserid(@Bind("userid") int userid);
+    User findByUserid(@Bind("userid") long userid);
 
     @SqlQuery("select * from user where surname = :surname")
     User findBySurname(@Bind("surname") String surname);
-    
+
+    @SqlQuery("select * from user where username = :username")
+    User findByUsername(@Bind("username") String username);
+
+    @SqlQuery("select * from user")
+    List<User> findAll();
+
 }
--- src/main/java/wicket/resources/UserResource.java
+++ src/main/java/wicket/resources/UserResource.java
@@ -2,11 +2,13 @@ package wicket.resources;
 
 import com.codahale.metrics.annotation.Timed;
 import io.dropwizard.hibernate.UnitOfWork;
+import io.dropwizard.jersey.params.LongParam;
 import wicket.core.entity.User;
 import wicket.db.jdbi.UserQueries;
 
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
+import java.util.List;
 import java.util.Optional;
 import java.util.concurrent.atomic.AtomicLong;
 
@@ -14,7 +16,7 @@ import java.util.concurrent.atomic.AtomicLong;
 @Produces(MediaType.APPLICATION_JSON)
 public class UserResource {
     private final UserQueries dao;
-    private final AtomicLong counter;
+    private final AtomicLong counter;  // FIXME!!
 
     public UserResource(UserQueries dao) {
         this.dao = dao;
@@ -22,16 +24,41 @@ public class UserResource {
     }
 
     @GET
-    @Path("/{surname}")
+    @Path("/surname/{surname}")
     @Timed
     @UnitOfWork
     public User findUserBySurname(@PathParam("surname") Optional<String> surname) {
         // mappa till DTO?
-        if (surname.isPresent()) {
-            return dao.findBySurname(surname.get());
-        } else {
-            return new User("inget");
-        }
+        return dao.findBySurname(surname.orElse("Nope"));
+
+    }
+
+    @GET
+    @Path("/{userid}")
+    @Timed
+    @UnitOfWork
+    public User findUserByUserid(@PathParam("userid") LongParam userid) {
+        // mappa till DTO?
+        return dao.findByUserid(userid.get());
+
+    }
+
+    @GET
+    @Timed
+    @UnitOfWork
+    public List<User> findAllUsers() {
+        // mappa till DTO?
+        return dao.findAll();
+
+    }
+
+    @GET
+    @Path("/username/{username}")
+    @Timed
+    @UnitOfWork
+    public User findUserByUsername(@PathParam("username") Optional<String> username) {
+        // mappa till DTO?
+        return dao.findByUsername(username.orElse("Nope"));
 
     }
 }
